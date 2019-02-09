e31d19ffda40ece67459adf505174b948ead1cc3
J_Aydin <janberd.aydin@gmail.com>
2019-02-09 20:31:10 +0100

    Login enzovoort

--- classes/User.php
+++ classes/User.php
@@ -8,13 +8,55 @@ class User
 
     public function regUser()
     {
-        $sql = 'INSERT INTO users(gebruikersnaam, email, wachtwoord) VALUES (:gebruikersnaam, :email, :wachtwoord)';
+        $sql = 'INSERT INTO users(naam, email, wachtwoord) VALUES (:naam, :email, :wachtwoord)';
 
         $stmt = DB::get()->prepare($sql);
 
-        $stmt->bindValue(':gebruikersnaam', $_POST['gebruikersnaam'], PDO::PARAM_STR);
+        $stmt->bindValue(':naam', $_POST['naam'], PDO::PARAM_STR);
         $stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
         $stmt->bindValue(':wachtwoord', password_hash($_POST['wachtwoord'], PASSWORD_BCRYPT), PDO::PARAM_STR);
         $stmt->execute();
+    }
+
+    public function logUser()
+    {
+        //Haalt enige whitespaces weg uit de inputs
+        $naam = !empty($_POST['naam']) ? trim($_POST['naam']) : null;
+        $wachtwoord= !empty($_POST['wachtwoord']) ? trim($_POST['wachtwoord']) : null;
+
+        //De database gegevens (tables en colums) worden aan een variable toegevoegd
+        $sql = "SELECT user_id, naam, wachtwoord FROM users WHERE naam=:naam";
+
+        //De connectie met de database wordt aan een variable toegevoegd.
+        $stmt = DB::get()->prepare($sql);
+
+        //De ingevulde value worden gebruikt om de username te controleren in de volgende if statement.
+        $stmt->bindValue(':naam', $naam);
+        $stmt->execute();
+
+        //Haalt de bijbehorende username op
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Controleert of de ingevulde username en wachtwoord overeenkomen met degene uit de database
+        if($user === false)
+        {
+            header("Location: login.php?wrongcombination");
+            exit();
+        } else
+        {
+            //Voeg de ingevulde value toe aan de variable $validPassword
+            $validPassword = password_verify($wachtwoord, $user['wachtwoord']);
+
+            //Controleert of de ingevulde wachtwoord overeenkomt met de hash in de database.
+            if($validPassword)
+            {
+                $_SESSION['id'] = $user['user_id'];
+                $_SESSION['logged_in'] = time();
+
+                header("Location: index.php?login=success");
+                exit();
+
+            }
+        }
     }
 }
--- login.php
+++ login.php
@@ -1,38 +1,46 @@
 <?php
 
 require_once 'classes/DB.class.php';
+require_once 'classes/User.php';
+
+if(isset($_POST['submitLogin']))
+{
+    $login = new User();
+    $login->logUser();
+}
+
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Quiz</title>
+    <title>Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="Style/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
-
 </head>
 <body>
-<h1>Index Pagina</h1>
 <div class="container">
-    <div class="row">
-        <div class="col-lg-6">
-            <a href="login.php" class="btn btn-primary">Inloggen</a>
-            <a href="registratie.php" class="btn btn-primary">Registreren</a>
+    <div class="modal-dialog">
+        <div class="loginmodal-container">
+            <a href="index.php" class="btn btn-primary">Home</a>
+            <h1>Login</h1><br>
+            <form method="post">
+                <input type="text" name="naam" placeholder="Gebruikersnaam" required>
+                <input type="password" name="wachtwoord" placeholder="Wachtwoord" required>
+                <button type="submit" name="submitLogin" class="login loginmodal-submit">Login</button>
+            </form>
         </div>
     </div>
 </div>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script>
-
-</script>
 </body>
 </html>
