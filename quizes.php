f6792f4b983dd2dd802231b7860442ae7fd9d2da
casperSteinebach <39789936+casperSteinebach@users.noreply.github.com>
2019-02-11 13:19:01 +0100

    Quiz pagina
    Quiz pagina toegevoegd.

---
+++ quizes.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+include "navbar.php";
+?>
+<div class="container mt-lg-4">
+    <div class="row">
+        <div class="col-md-4" class="rounded" align="center">
+             <a href="#">Quiz 1</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+        <div class="col-md-4" class="rounded" align="center">
+            <a href="#">Quiz 2</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+        <div class="col-md-4" class="rounded" align="center" >
+            <a href="#">Quiz 3</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+    </div>
+    <div class="row mt-md-5">
+        <div class="col-md-4" class="rounded" align="center">
+            <a href="#">Quiz 4</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+        <div class="col-md-4" class="rounded" align="center">
+            <a href="#">Quiz 5</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+        <div class="col-md-4"  class="rounded" align="center">
+            <a href="#">Quiz 6</a>
+            <br>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique leo non leo ullamcorper efficitur.</p>
+        </div>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
