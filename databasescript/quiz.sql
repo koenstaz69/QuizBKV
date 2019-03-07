f8008304c3b4efda8f74ef2fffc3593ab2db3320
koenstaz69 <39735657+koenstaz69@users.noreply.github.com>
2019-03-07 13:41:27 +0100

    Update quiz.sql

--- databasescript/quiz.sql
+++ databasescript/quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 08 feb 2019 om 13:51
+-- Gegenereerd op: 07 mrt 2019 om 13:40
 -- Serverversie: 10.1.37-MariaDB
 -- PHP-versie: 7.3.1
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `gebruikersnaam` varchar(255) NOT NULL,
+  `naam` varchar(255) NOT NULL,
   `email` varchar(11) NOT NULL,
   `wachtwoord` varchar(255) NOT NULL,
   `rol` int(1) NOT NULL
@@ -40,9 +40,11 @@ CREATE TABLE `users` (
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`user_id`, `gebruikersnaam`, `email`, `wachtwoord`, `rol`) VALUES
+INSERT INTO `users` (`user_id`, `naam`, `email`, `wachtwoord`, `rol`) VALUES
 (3, 'gebruiker', 'test@gmail.', '$2y$10$VNoWtg4v8bQ.oOSZpl0yvuCIBwoL3HxbtBqeC33ygDpkT7IP83IC6', 0),
-(4, 'test1', 'test1@gmail', '$2y$10$F.tRjkNiEqXd467fuouwbeHk7ZBYB7dMP6hO.GTXcQjyU9BPmOBLO', 0);
+(4, 'test1', 'test1@gmail', '$2y$10$F.tRjkNiEqXd467fuouwbeHk7ZBYB7dMP6hO.GTXcQjyU9BPmOBLO', 0),
+(5, 'tetk', 'tekt@gmail.', '$2y$10$QZTmogR.N7/wqdEEzKxXM.0drYyqa93.3Z1TcGu.jBVXx1QTl56qa', 0),
+(6, 'koentje', 'koenstaz@gm', '$2y$10$9iWDpjhR5m.bv29Okmbi3uc0KuG8Uo6IpdR/u4bIPUNnWprSMzQ3.', 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -62,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
