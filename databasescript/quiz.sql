343a8b60fc7e04e789f1e507df497e463849f4a2
koenstaz69 <39735657+koenstaz69@users.noreply.github.com>
2019-03-28 14:40:26 +0100

    Update quiz.sql

--- databasescript/quiz.sql
+++ databasescript/quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 07 mrt 2019 om 13:40
+-- Gegenereerd op: 28 mrt 2019 om 14:39
 -- Serverversie: 10.1.37-MariaDB
 -- PHP-versie: 7.3.1
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `quiz`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `quiz_id` int(7) NOT NULL,
+  `gebruiker_id` int(7) NOT NULL,
+  `onderwerp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `quizronde`
+--
+
+CREATE TABLE `quizronde` (
+  `quizronde_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,9 +71,39 @@ INSERT INTO `users` (`user_id`, `naam`, `email`, `wachtwoord`, `rol`) VALUES
 (5, 'tetk', 'tekt@gmail.', '$2y$10$QZTmogR.N7/wqdEEzKxXM.0drYyqa93.3Z1TcGu.jBVXx1QTl56qa', 0),
 (6, 'koentje', 'koenstaz@gm', '$2y$10$9iWDpjhR5m.bv29Okmbi3uc0KuG8Uo6IpdR/u4bIPUNnWprSMzQ3.', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `vragen`
+--
+
+CREATE TABLE `vragen` (
+  `vraag_id` int(7) NOT NULL,
+  `quiz_id` int(7) NOT NULL,
+  `vraag` varchar(255) NOT NULL,
+  `antwoord1` varchar(255) NOT NULL,
+  `antwoord2` varchar(255) NOT NULL,
+  `antwoord3` varchar(255) NOT NULL,
+  `antwoord4` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quiz_id`);
+
+--
+-- Indexen voor tabel `quizronde`
+--
+ALTER TABLE `quizronde`
+  ADD PRIMARY KEY (`quizronde_id`),
+  ADD KEY `quiz_id` (`quiz_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexen voor tabel `users`
@@ -57,14 +112,50 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexen voor tabel `vragen`
+--
+ALTER TABLE `vragen`
+  ADD PRIMARY KEY (`vraag_id`),
+  ADD KEY `quiz_id` (`quiz_id`);
+
+--
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
+
+--
+-- AUTO_INCREMENT voor een tabel `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `quiz_id` int(7) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT voor een tabel `vragen`
+--
+ALTER TABLE `vragen`
+  MODIFY `vraag_id` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `quizronde`
+--
+ALTER TABLE `quizronde`
+  ADD CONSTRAINT `quizronde_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`),
+  ADD CONSTRAINT `quizronde_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Beperkingen voor tabel `vragen`
+--
+ALTER TABLE `vragen`
+  ADD CONSTRAINT `vragen_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
